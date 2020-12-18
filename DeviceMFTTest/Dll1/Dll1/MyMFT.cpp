1babcf434e66cadf24ff2b790ce6b69db576d8ab
Masaru Nishiyama <mnishiyama@lenovo.com>
2020-12-18 14:16:30 +0900

    Add comment to show Stackoverflow URL.

--- DeviceMFTTest/Dll1/Dll1/MyMFT.cpp
+++ DeviceMFTTest/Dll1/Dll1/MyMFT.cpp
@@ -10,6 +10,7 @@ MyMFT::MyMFT()
 {
 }
 
+// https://stackoverflow.com/questions/21431047/how-to-convert-guid-to-char?rq=1
 std::string guidToString(GUID guid) {
     std::array<char, 40> output;
     snprintf(output.data(), output.size(), "{%08X-%04hX-%04hX-%02X%02X-%02X%02X%02X%02X%02X%02X}", guid.Data1, guid.Data2, guid.Data3, guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3], guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]);
